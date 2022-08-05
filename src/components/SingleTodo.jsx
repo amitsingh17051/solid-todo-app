21dd330dc3540a6371b0329f2525b6d31e666787
unknown <amitsingh17051@gmail.com>
2022-08-05 11:53:56 +0530

    Added task delete button

--- src/components/SingleTodo.jsx
+++ src/components/SingleTodo.jsx
@@ -1,13 +1,12 @@
 
-export default function SingleTodo({todo, handleChangeChk}) {
+export default function SingleTodo({todo, handleChangeChk, DeleteTask}) {
     return (
         <>
             <li data-todo-id={todo.id}>
                 <label class={ todo.completed ? 'text-decoration-line-through' : '' } for={`${todo.id}-checked`}> {todo.name}</label>
-               
                 <input id={`${todo.id}-checked`} type="checkbox"  checked={ todo.completed ? 'checked' : '' } onChange={handleChangeChk} />
-                
+                <button onClick={DeleteTask} >Delete</button>
             </li>
         </> 
     )
-}
+} 
--- src/components/TodoLists.jsx
+++ src/components/TodoLists.jsx
@@ -6,39 +6,39 @@ export default function TodoLists() {
     const [Todos, SetTodos] = createSignal([
         {
             name:"Todo One",
-            id: 1,
+            id: 111,
             completed: false
         },
         {
             name:"Todo Two",
-            id: 2,
+            id: 222,
             completed: true
         },
         {
             name:"Todo Three",
-            id: 3,
+            id: 333,
             completed: false
         }
     ]);
 
    
     function handleChangeChk(e) {
-        const checkedItemID = e.target.parentNode.getAttribute("data-todo-id");
+        let checkedItemID = e.target.parentNode.getAttribute("data-todo-id");
        
-            var UpdatedTodos = Todos().map(item => {
-                if(item.id == checkedItemID) {
-                   if(e.target.checked) {
-                    item.completed = true;
-                    return item;
-                   } else {
-                    item.completed = false;
-                    return item;
-                   }
-                }
-
+        let UpdatedTodos = Todos().map(item => {
+            if(item.id == checkedItemID) {
+                if(e.target.checked) {
+                item.completed = true;
                 return item;
-            }) 
-            SetTodos(UpdatedTodos); 
+                } else {
+                item.completed = false;
+                return item;
+                }
+            }
+
+            return item;
+        }) 
+        SetTodos(UpdatedTodos); 
     }
     
 
@@ -46,7 +46,7 @@ export default function TodoLists() {
         e.preventDefault();
         const newTodo = {};
         newTodo["name"] = e.target.elements[0].value;
-        newTodo["id"] = Todos().length + 1;
+        newTodo["id"] = new Date().getTime();
         newTodo["completed"] = false;
         Todos().push(newTodo);
 
@@ -54,19 +54,30 @@ export default function TodoLists() {
         handleChangeChk(e)  
     }
 
- 
-
+    function DeleteTask(e) {
+        let checkedItemID = e.target.parentNode.getAttribute("data-todo-id");
+        let itemIndex = 0;
+        let UpdatedTodos = Todos().map((item, i) => {
+            if(item.id == checkedItemID) {
+                itemIndex = i;
+            }
+            return item;
+        }) 
+        UpdatedTodos.splice(itemIndex, 1);
+        SetTodos(UpdatedTodos); 
+    }
 
     return (
         <>
             <div class="todos">
                 <ul>
                     { Todos().map( item => {
-                        return <SingleTodo todo={item} handleChangeChk={handleChangeChk} />;
+                        return <SingleTodo todo={item} handleChangeChk={handleChangeChk} DeleteTask={DeleteTask}/>;
                     }) }
                 </ul>
             </div>
             <AddTodo addTodo={addTodo}/>
         </>
     )
+
 }
